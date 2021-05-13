fe412beac1d697f898ef623b4e8e82a8d14b3a64
Shrishti <shrishti2454soam@gmail.com>
2021-05-13 14:50:30 +0530

    changes

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var mongo = require('mongodb');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var shortid = require('shortid');
+var dns = require('dns');
+var urlparser = require('url');
 var express = require('express');
 var cors = require('cors');
 var app = express();
@@ -30,34 +32,54 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 let suffix = shortid.generate();
+
 app.post("/api/shorturl",function(req,res){
   console.log("post req called");
   console.log(req.body.url+"req body");
+
   let newURL = new ShortUrl({
     "short_url" : __dirname + "/api/shorturl/" + suffix,
     "original_url" : req.body.url,
     "suffix" : suffix
   })
-  newURL.save(function(err,doc){
-    if(err){
-      console.log(err);
+  const bodyurl = req.body.url ; 
+  const something = dns.lookup(urlparser.parse(bodyurl).hostname,(error,address)=>{
+    if(!address){
+      res.json({error:"Invalid URL"})
+    }else{
+      newURL.save(function(err,doc){
+        if(err){
+          console.log(err);
+        }
+        res.json({
+          "short_url":newURL.short_url,
+          "original_url":newURL.original_url,
+          "suffix":newURL.suffix
+        });
+      });
     }
-    res.json({
-      "short_url":newURL.short_url,
-      "original_url":newURL.original_url,
-      "suffix":newURL.suffix
-    });
-  });
+  })
+  
 })
 app.get("/api/shorturl/:suffix",function(req,res){
   let userGeneratedSuffix = req.params.suffix;
-  ShortUrl.find({suffix:userGeneratedSuffix}).then(function(foundUrl){
-    
-    let userRedirect = foundUrl[0];
-    // console.log(userRedirect);
-    res.redirect(userRedirect.original_url);
+  ShortUrl.findById({userGeneratedSuffix},(err,data)=>{
+    console.log(data.original_url);
+    if(!data){
+      res.json({
+        error : "Invalid URL"
+      })
+    }else{
+      res.redirect(data.original_url);
+    }
   })
-})
+});
+    
+//     let userRedirect = foundUrl[0];
+//     // console.log(userRedirect);
+//     res.redirect(userRedirect.original_url);
+//   })
+// })
 
 
 // Your first API endpoint
